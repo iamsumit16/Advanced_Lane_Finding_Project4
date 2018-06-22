8a828ab29d206d9112eeeaee44bf0bbc442fe020
iamsumit16 <sumitch@umich.edu>
2018-06-22 02:03:03 -0400

    Update p4.py

--- p4.py
+++ p4.py
@@ -1,1 +1,53 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+import cv2
+%matplotlib inline
+import collections
+import glob
 
+
+
+#Read calibration images
+camera_cal_location = glob.glob('camera_cal/*.jpg')
+cal_images = []
+for f in camera_cal_location:
+    img = mpimg.imread(f)
+    cal_images.append(img)
+print(len(cal_images), cal_images[0].shape)
+nx = 9
+ny = 6
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((nx*ny,3), np.float32)
+objp[:,:2] = np.mgrid[0:nx, 0:ny].T.reshape(-1,2)
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d points in real world space
+imgpoints = [] # 2d points in image plane.
+
+for image in cal_images:
+    gray = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
+    ret, corners = cv2.findChessboardCorners(gray, (nx,ny), None)
+    if ret == True:
+        objpoints.append(objp)
+        imgpoints.append(corners)
+        cv2.drawChessboardCorners(image, (nx, ny), corners, ret)
+      
+      
+      
+def undistort_img(img, objpoints, imgpoints):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None,None)
+    undist = cv2.undistort(img, mtx, dist, None, mtx)
+    return undist
+
+def abs_sobel_thresh(img, orient='x', sobel_kernel=5, thresh=(0,255)):
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY) 
+    if orient=='x': abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel))
+    if orient=='y': abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel))
+    scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel)) # rescale 8-bit
+    sobel_binary = np.zeros_like(scaled_sobel) # create a copy and apply threshold
+    sobel_binary[(scaled_sobel >= thresh[0]) & (scaled_sobel <= thresh[1])] = 1
+    return sobel_binary
+
+def 
